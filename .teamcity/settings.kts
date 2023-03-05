f0a4bdfa53db3c29ab3d223c562411fed98597f5
Vitalii Bedletskyi <vitalii.bedletskyi@binary-studio.com>
2023-03-05 20:34:51 +0200

    revert previous configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,8 @@
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.nodeJS
 import jetbrains.buildServer.configs.kotlin.projectFeatures.buildReportTab
 import jetbrains.buildServer.configs.kotlin.projectFeatures.githubConnection
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -49,10 +51,65 @@ project {
         }
     }
 
-    subProject(Hackolade)
+    subProject(HackoladePlugins)
 }
 
+object HackoladePlugins : Project({
+    template(PluginBuildTemplate)
 
-object Hackolade : Project({
-    name = "Hackolade"
+    features {
+        githubConnection {
+            id = "PROJECT_EXT_2"
+            displayName = "GitHub.com"
+            clientId = "cb10c5eb655b55614a2e"
+            clientSecret = "credentialsJSON:6d2e09f0-6472-411a-97ff-2bb6f4cf06f1"
+        }
+    }
+
+    subProject(Plugin)
+})
+
+object Plugin : Project({
+    name = DslContext.projectName
+
+    vcsRoot(PluginRepo)
+
+    buildType(PluginBuild)
+})
+
+object PluginRepo : GitVcsRoot({
+    name = DslContext.getParameter("repoName")
+    url = DslContext.getParameter("repoHttpUrl")
+    branch = DslContext.getParameter("mainBranch")
+    branchSpec = "refs/heads/*"
+})
+
+object PluginBuildTemplate : Template({
+    name = "Build"
+
+    steps {
+        nodeJS {
+            name = "Instal dependecies"
+            id = "RUNNER_4"
+            shellScript = "npm ci"
+            dockerImage = "node:16"
+        }
+        nodeJS {
+            name = "Run Linter"
+            id = "RUNNER_5"
+            shellScript = "npm run lint"
+            dockerImage = "node:16"
+        }
+        nodeJS {
+            name = "Package plugin"
+            id = "RUNNER_6"
+            shellScript = "npm run package"
+            dockerImage = "node:16"
+        }
+    }
+})
+
+object PluginBuild : BuildType({
+    templates(PluginBuildTemplate)
+    name = "Build"
 })
