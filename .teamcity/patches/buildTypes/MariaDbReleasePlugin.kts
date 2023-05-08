feac993c815bddc0168c10f9fe20d6791339d501
bedletskyi <bedletskyi@TeamCity>
2023-05-08 19:56:25 +0000

    TeamCity change in 'Hackolade Plugins / MariaDB' project: runners of 'Release plugin' build configuration were updated

--- .teamcity/patches/buildTypes/MariaDbReleasePlugin.kts
+++ .teamcity/patches/buildTypes/MariaDbReleasePlugin.kts
@@ -79,7 +79,7 @@ changeBuildType(RelativeId("MariaDbReleasePlugin")) {
             script {
                 name = "Build plugin and upload artifact to azure and dockerhub"
                 scriptContent = """
-                    ssh-keyscan github.com >> ~/.ssh/known_hosts
+                    mkdir -p -m 0600 ~/.ssh && ssh-keyscan github.com >> ~/.ssh/known_hosts
                     ssh -T git@github.com
                     
                     #docker buildx bake -f ./ci-cd/plugins/docker-bake.hcl release
