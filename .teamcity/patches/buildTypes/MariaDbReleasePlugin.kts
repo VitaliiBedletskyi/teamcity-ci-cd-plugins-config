c8d3176a4deabe596a48601537c41e210e966204
bedletskyi <bedletskyi@TeamCity>
2023-05-08 17:54:24 +0000

    TeamCity change in 'Hackolade Plugins / MariaDB' project: parameters of 'Release plugin' build configuration were updated

--- .teamcity/patches/buildTypes/MariaDbReleasePlugin.kts
+++ .teamcity/patches/buildTypes/MariaDbReleasePlugin.kts
@@ -15,7 +15,7 @@ changeBuildType(RelativeId("MariaDbReleasePlugin")) {
             text("env.RELEASE_TITLE", "", label = "Release title", display = ParameterDisplay.PROMPT, allowEmpty = true)
         }
         add {
-            param("CURRENT_PLUGIN_VERSION", "", "webPopulatedSelect", "method" to "GET", "display" to "prompt", "format" to "json", "readOnly" to "true", "label" to "Current plugin version", "url" to "https://testpluginstorage.blob.core.windows.net/plugins/MariaDB/currentReleaseVersion.json", "enableEditOnError" to "true")
+            param("env.CURRENT_PLUGIN_VERSION", "", "webPopulatedSelect", "method" to "GET", "display" to "prompt", "format" to "json", "readOnly" to "true", "label" to "Current plugin version", "url" to "https://testpluginstorage.blob.core.windows.net/plugins/MariaDB/currentReleaseVersion.json", "enableEditOnError" to "true")
         }
         add {
             param("env.NEXT_DEVELOPMENT_VERSION", "", "webPopulatedSelect", "method" to "GET", "display" to "prompt", "format" to "json", "label" to "Next development version", "url" to "https://testpluginstorage.blob.core.windows.net/plugins/MariaDB/nextDevelopmentVersion.json", "enableEditOnError" to "true")
