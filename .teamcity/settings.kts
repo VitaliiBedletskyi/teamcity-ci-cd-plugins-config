972fcd4afa1f5b6f63c2c2eb0ba533c0193986ad
bedletskyi <bedletskyi@TeamCity>
2023-07-14 11:03:35 +0000

    TeamCity change in 'Test' project: 'Test build' build configuration was created

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -49,9 +49,15 @@ project {
 object Test : Project({
     name = "Test"
 
+    buildType(Test_TestBuild)
+
     params {
         text("env.RELEASE_DESCRIPTION", "", label = "Release description", display = ParameterDisplay.PROMPT, allowEmpty = true)
         text("env.RELEASE_TITILE", "", label = "Release titile", display = ParameterDisplay.PROMPT, allowEmpty = true)
         param("env.RELEASE_VERSION", "", "webPopulatedSelect", "method" to "GET", "display" to "prompt", "format" to "json", "readOnly" to "true", "label" to "Release version", "url" to "https://testpluginstorage.blob.core.windows.net/plugins/MariaDB/currentReleaseVersion.json")
     }
+})
+
+object Test_TestBuild : BuildType({
+    name = "Test build"
 })
